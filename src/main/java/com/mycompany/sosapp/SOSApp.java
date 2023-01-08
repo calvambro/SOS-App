14a67d889499680fa81e024045dc8dc3f97150c8
ambrosius calvin <calvambro@gmail.com>
2023-01-08 17:08:11 +0700

    initial commit

---
+++ src/main/java/com/mycompany/sosapp/SOSApp.java
@@ -0,0 +1,12 @@
+package com.mycompany.sosapp;
+
+/**
+ *
+ * @author calva
+ */
+public class SOSApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
